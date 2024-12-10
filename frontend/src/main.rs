758c80ba4e239368f257260958d87f31dda0acc9
Anatol Ulrich <e+github@mail.taugt.net>
2024-12-10 13:10:33 +0100

    idiomatic: document::Stylesheet instead of document::Link

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -33,9 +33,8 @@ fn App() -> Element {
     rsx! {
         // Global app resources
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-
+        document::Stylesheet { href: MAIN_CSS }
+        document::Stylesheet { href: TAILWIND_CSS }
         Router::<Route> {}
     }
 }
